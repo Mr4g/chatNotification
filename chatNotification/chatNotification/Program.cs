5030d1c191fa9ccdde1d4d5ddd8b2703d06f292c
SmbL <smbl@viessmann.com>
2023-06-01 08:02:22 +0200

    v1.1

--- chatNotification/chatNotification/Program.cs
+++ chatNotification/chatNotification/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using PuppeteerSharp;
 
@@ -9,87 +8,109 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        string webhookUrl = "https://chat.googleapis.com/v1/spaces/AAAAP2eoOa4/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=-yD9u_9y6_YIBcQ8t6zNPsIEUDCBINkyUg2nejkooGA"; // Zastąp adresem webhooku Google Chat
-        string screenshotPath = "C:/zrzut_ekranu.png"; // Ścieżka, gdzie zostanie zapisany zrzut ekranu
 
-        await WyslijWiadomoscDoGoogleChat(webhookUrl, "Wymiana butli");
+
+
+
+        string screenshotPath = "C:/zrzut_ekranu.png"; // Ścieżka, gdzie zostanie zapisany zrzut ekranu
+        string imgurClientId = "0fe6e59673311dc"; // Zastąp wartością swojego Client ID zarejestrowanego na Imgur
+        string chatWebhookUrl = "https://chat.googleapis.com/v1/spaces/AAAAP2eoOa4/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=-yD9u_9y6_YIBcQ8t6zNPsIEUDCBINkyUg2nejkooGA"; // Zastąp wartością swojego URL webhooka do Chat Google
+
+        string imagePath = "C:/zrzut_ekranu.png"; // Zastąp ścieżką do swojego pliku z obrazem
 
         await ZrobZrzutEkranu(screenshotPath);
 
-        await WyslijZrzutEkranuDoGoogleChat(webhookUrl, screenshotPath);
-
-        Console.WriteLine("Proces zakończony.");
-
-        // Możesz również usunąć zrzut ekranu po wysłaniu, jeśli nie jest już potrzebny
-        // File.Delete(screenshotPath);
-    }
-
-    static async Task WyslijWiadomoscDoGoogleChat(string webhookUrl, string wiadomosc)
-    {
-        using (var httpClient = new HttpClient())
+        static async Task ZrobZrzutEkranu(string sciezkaZrzutu)
         {
-            var tresc = new StringContent($"{{\"text\":\"{wiadomosc}\"}}");
-            tresc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-            var odpowiedz = await httpClient.PostAsync(webhookUrl, tresc);
-
-            if (odpowiedz.IsSuccessStatusCode)
+            var opcjeStartowe = new LaunchOptions
             {
-                Console.WriteLine("Wiadomość została wysłana na Google Chat!");
-            }
-            else
-            {
-                Console.WriteLine("Wystąpił błąd podczas wysyłania wiadomości na Google Chat.");
-            }
-        }
-    }
-
-    static async Task ZrobZrzutEkranu(string sciezkaZrzutu)
-    {
-        var opcjeStartowe = new LaunchOptions
-        {
-            ExecutablePath = "C:/Program Files/Google/Chrome/Application/chrome.exe", // Zastąp ścieżką do pliku wykonywalnego Chromium na Twoim komputerze
-            Headless = true
-        };
-
-        await using (var przegladarka = await Puppeteer.LaunchAsync(opcjeStartowe))
-        {
-            var strona = await przegladarka.NewPageAsync();
-            await strona.GoToAsync("https://w6228w05.viessmann.net:8000/en-GB/app/VI_W16/r410_measurement?form.tok_time.earliest=-4h%40h&form.tok_time.latest=now&form.tok_res=15&form.tok_t_unit=s");
-
-            // Opóźnienie wykonania zrzutu ekranu o 10 sekund
-            await Task.Delay(15000);
-
-            var zrzutEkranuOpcje = new ScreenshotOptions
-            {
-                FullPage = true
+                ExecutablePath = "C:/Program Files/Google/Chrome/Application/chrome.exe", // Zastąp ścieżką do pliku wykonywalnego Chromium na Twoim komputerze
+                Headless = true
             };
 
-            await strona.ScreenshotAsync(sciezkaZrzutu, zrzutEkranuOpcje);
+            await using (var przegladarka = await Puppeteer.LaunchAsync(opcjeStartowe))
+            {
+                var strona = await przegladarka.NewPageAsync();
+                await strona.GoToAsync("https://w6228w05.viessmann.net:8000/en-GB/app/VI_W16/r410_measurement?form.tok_time.earliest=-4h%40h&form.tok_time.latest=now&form.tok_res=15&form.tok_t_unit=s");
+
+                // Opóźnienie wykonania zrzutu ekranu o 10 sekund
+                Console.WriteLine("Oczekiwanie na screen...");
+                await Task.Delay(15000);
+
+
+                var zrzutEkranuOpcje = new ScreenshotOptions
+                {
+                    FullPage = true
+                };
+
+                await strona.ScreenshotAsync(sciezkaZrzutu, zrzutEkranuOpcje);
+            }
+
+            Console.WriteLine("Zrzut ekranu został zapisany.");
         }
 
-        Console.WriteLine("Zrzut ekranu został zapisany.");
-    }
 
-    static async Task WyslijZrzutEkranuDoGoogleChat(string webhookUrl, string sciezkaZrzutu)
-    {
+
         using (var httpClient = new HttpClient())
-        using (var formularz = new MultipartFormDataContent())
-        using (var strumienPliku = File.OpenRead(sciezkaZrzutu))
         {
-            formularz.Add(new StreamContent(strumienPliku), "image", "zrzut_ekranu.png");
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Client-ID {imgurClientId}");
 
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("multipart/form-data"));
-            var odpowiedz = await httpClient.PostAsync(webhookUrl, formularz);
-
-            if (odpowiedz.IsSuccessStatusCode)
+            byte[] imageData;
+            using (var fileStream = File.OpenRead(imagePath))
             {
-                Console.WriteLine("Zrzut ekranu został wysłany na Google Chat!");
+                imageData = new byte[fileStream.Length];
+                await fileStream.ReadAsync(imageData, 0, imageData.Length);
+            }
+
+            var content = new MultipartFormDataContent();
+            content.Add(new ByteArrayContent(imageData), "image", "image.jpg");
+
+            var response = await httpClient.PostAsync("https://api.imgur.com/3/image", content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Pobranie linku do przesłanego obrazu z Imgur
+                var imgurResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<ImgurResponse>(responseContent);
+                string imageUrl = imgurResponse.Data.Link;
+
+                // Wysłanie wiadomości na Chat Google z linkiem do obrazu
+                using (HttpClient chatClient = new HttpClient())
+                {
+                    var chatMessage = new
+                    {
+                        text = $"WYCIEK R410!!: {imageUrl}"
+                    };
+
+                    var json = Newtonsoft.Json.JsonConvert.SerializeObject(chatMessage);
+                    var chatContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage chatResponse = await chatClient.PostAsync(chatWebhookUrl, chatContent);
+
+                    if (chatResponse.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Wiadomość została wysłana z linkiem do obrazu.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wystąpił błąd podczas wysyłania wiadomości na Chat Google.");
+                    }
+                }
             }
             else
             {
-                Console.WriteLine("Wystąpił błąd podczas wysyłania zrzutu ekranu na Google Chat.");
+                Console.WriteLine("Wystąpił błąd podczas przesyłania obrazu do Imgur.");
             }
         }
+    }
+
+    class ImgurResponse
+    {
+        public ImgurData Data { get; set; }
+    }
+
+    class ImgurData
+    {
+        public string Link { get; set; }
     }
 }
